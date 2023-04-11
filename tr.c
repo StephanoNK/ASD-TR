cd4ceb7f0bb0fd970dde713392aafe0d5d5e493a
StephanoNK <stephanonovk@gmail.com>
2023-04-11 16:17:25 +0700

    a

--- tr.c
+++ tr.c
@@ -5,10 +5,10 @@
 #include <string.h>
 #include <conio.h>
 
-void loading(),login(), menu_admin(), menu_utama();
+void loading(),login(), menu_admin(), menu_utama(), create();
 
 struct mobil{
-    char ID[5], nama[100], warna[30];
+    char ID[7], merk[50], nama[100], warna[30];
     int tahun;
     long harga;
     struct mobil* prev;
@@ -166,3 +166,34 @@ void menu_admin(){
     }
 }
 
+void create() {
+	system("cls");
+	struct mobil* ptr, * temp;
+	ptr = (struct mobil*)malloc(sizeof(struct mobil));
+	if (ptr == NULL) {
+		printf("MEMORY FULL!");
+		getch();
+		return;
+	}
+	printf("\n\n\n\n\n\n\n\n");
+	printf("\t\t\t\t\t**********************************\n");
+	printf("\t\t\t\t\t**       MENU DEALER MOBIL      **\n");
+	printf("\t\t\t\t\t**********************************\n");
+	printf("\n");
+	printf("\n\t\t\t\t\t\tMasukkan Merk Mobil: ");
+	scanf("%[^\n]s", &ptr->merk);
+	while (getchar() != '\n');
+	printf("\n\t\t\t\t\t\tMasukkan Nama Mobil: ");
+	scanf("%[^\n]s", &ptr->nama);
+	while (getchar() != '\n');
+	printf("\n\t\t\t\t\t\tMasukkan Warna Mobil: ");
+	scanf("%[^\n]s", &ptr->warna);
+	while (getchar() != '\n');
+	printf("\n\t\t\t\t\t\tMasukkan Tahun Mobil: ");
+	scanf("%d", &ptr->tahun);
+	while (getchar() != '\n');
+	printf("\n\t\t\t\t\t\tMasukkan Harga Mobil: ");
+	scanf("%ld", &ptr->harga);
+	while (getchar() != '\n');
+
+}
